8d274adfb4bb87a253c7691095113ef5494147be
corybrunson <cornelioid@gmail.com>
2020-05-21 18:52:56 -0400

    first commit: package infrastructure

---
+++ R/landmarks-lastfirst-yara.r
@@ -0,0 +1,68 @@
+#' @title Firstlast-generated Landmarks
+#' @description Computes a landmark set based on nearest neighborhoods.
+#' @details This function adapts the maxmin procedure to produce landmark points
+#'   dispersed according to the orders in which they are reached from each
+#'   other, rather than to their distances from each other. (Say more.)
+#' @param x a data matrix.
+#' @param n the number of landmarks requested.
+#' @param k the desired cardinality of each cover set.
+#' @param dist_method the distance metric to use. Any distance measure in the
+#'   \code{proxy} package is supported.
+#' @param seed_index the first landmark to seed the algorithm.
+#' @param shuffle_data whether to first randomly shuffle the data.
+#' @export
+get_landmarks_lastfirst_yara <- function(
+  x, n=NULL, k=NULL,
+  dist_method = "euclidean", seed_index = 1, shuffle_data=FALSE
+){
+  stopifnot(is.matrix(x))
+  stopifnot(seed_index >= 1L && seed_index <= nrow(x))
+  stopifnot(!is.null(n) || !is.null(eps)) # must specify a number of balls or a radius
+
+  shuffle_idx <- NA
+  if (shuffle_data){
+    shuffle_idx <- sample(seq(nrow(x)))
+    x <- x[,,drop=FALSE]
+  }
+
+  if(!is.null(n)){
+    if (missing(dist_method) || toupper(dist_method) == "EUCLIDEAN"){
+      landmark_idx <- landmark_maxmin(x, n, seed_index-1L)
+    } else if (requireNamespace("proxy", quietly = TRUE)){
+      stopifnot(toupper(dist_method) %in% toupper(proxy::pr_DB$get_entry_names()))
+      landmark_idx <- vector(mode="integer", n)
+      landmark_idx[1L] <- seed_index
+      landmark_min_dist <- rep(Inf, nrow(x))
+      for (i in 2L:n){
+        landmark_dist <- proxy::dist(x, x[landmark_idx[i-1L],,drop=FALSE], method = dist_method)
+        landmark_min_dist <- pmin(landmark_dist, landmark_min_dist)
+        potential_idx <- setdiff(seq(nrow(x)), landmark_idx[c(1:i)])
+        landmark_idx[i] <- potential_idx[which.max(landmark_min_dist[potential_idx])]
+      }
+    } else {
+      stop(sprintf("Unsupported distance method passed: %s\n", dist_method))
+    }
+  } else if(!is.null(eps)){
+    stopifnot(toupper(dist_method) %in% toupper(proxy::pr_DB$get_entry_names()))
+    f_dim <- ncol(x)
+    f_size <- nrow(x)
+
+    # algorithm and variable names as specified in Dlotko paper
+    C = c() # create a list to store indices of centers/landmarks
+    next_pt = seed_index # first landmark should be the seed point
+    while(TRUE){
+      C = append(C, next_pt) # add new point to list of landmarks
+
+      # compute distance between landmark set and each point in the space
+      dists = proxy::dist(matrix(x[C,], ncol=f_dim), x, method = dist_method)
+      sortedDists = matrix(apply(dists,2,sort),ncol=f_size)
+
+      # the next landmark is the point with greatest distance from the current landmark set
+      next_pt = which.max(sortedDists[1,])
+      d = sortedDists[1,next_pt] # done when this max distance is < eps, i.e. when all pts are contained in an eps-ball
+      if(d < eps){break}
+    }
+    landmark_idx = C
+  }
+  if (is.na(shuffle_idx)){ landmark_idx } else { shuffle_idx[landmark_idx] }
+}
---
+++ src/code.cpp
@@ -0,0 +1,59 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+#include <functional>
+#include <algorithm>
+
+using std::size_t;
+
+inline double sq_dist(const NumericVector& x, const NumericVector& y){
+  NumericVector diff = x-y;
+  return(sum(pow(diff, 2.0)));
+}
+
+// Eccentricity
+// Computes a distance measure from the barycenter of the data, or from a given
+// center, if provided.
+// [[Rcpp::export]]
+NumericVector eccentricity(const NumericMatrix& from, const NumericMatrix& x, const int type = 1) {
+  if (from.ncol() != x.ncol()){ stop("Matrices must have identical number of columns."); }
+  const size_t n_pts = x.nrow();
+  const size_t n_src_pts = from.nrow();
+  std::vector< double > pt_ecc(n_src_pts);
+  std::vector< double > pt_dist(n_pts);
+  for (size_t i = 0; i < n_src_pts; ++i){
+    for (size_t j = 0; j < n_pts; ++j){
+      pt_dist.at(j) = sq_dist(from.row(i), x.row(j));
+    }
+    pt_ecc.at(i) = accumulate(begin(pt_dist), end(pt_dist), 0.0, std::plus< double >())/n_pts;
+  }
+  return(wrap(pt_ecc));
+}
+
+// Uses the euclidean maxmin procedure to choose n landmarks.
+// [[Rcpp::export]]
+IntegerVector landmark_maxmin(const NumericMatrix& x, const int n, const int seed = 0) {
+  const size_t n_pts = x.nrow();
+  std::vector< double > lm_dist(n_pts, std::numeric_limits<double>::infinity());
+  IntegerVector landmark_idx = no_init_vector(n);
+  //landmark_idx[seed] = 0;
+  landmark_idx[0] = seed;
+  IntegerVector::iterator c_landmark = landmark_idx.begin();
+  double new_min_dist;
+  std::generate(landmark_idx.begin()+1, landmark_idx.end(), [&](){
+    size_t i = 0;
+    new_min_dist = std::numeric_limits<double>::infinity();
+
+    // Replace nearest landmark distances
+    std::replace_if(lm_dist.begin(), lm_dist.end(), [&i, &x, &c_landmark, &new_min_dist](const double c_dist){
+      new_min_dist = sq_dist(x.row(*c_landmark), x.row(i++));
+      return(new_min_dist < c_dist);
+    }, new_min_dist);
+
+    // Find the point that maximizes said distances, move to next landmark
+    c_landmark++;
+    auto max_landmark = std::max_element(lm_dist.begin(), lm_dist.end());
+    return(std::distance(lm_dist.begin(), max_landmark));
+  });
+  return(landmark_idx+1); // 1-based return
+}
